e8210b1100b0f7094357f470234e42d7ea2c207f
osbrown1 <socknot@live.com>
2023-02-15 16:02:35 -0800

    semi-working markers on map, need to click in specific places.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import 'leaflet/src/Leaflet.js';;
 import './css/styles.css';
 import Marker from './markers.js';
 import buildMap from './maps';
+
  
 
 // Initialize the map
@@ -33,44 +34,75 @@ export function handleMap(){
     const lat = event.latlng.lat;
     const lng = event.latlng.lng;
 
-    // Create a new marker with name and coordinates
-    const marker = L.marker([lat, lng]).addTo(mymap);
 
-    // creates popup object for marker
-    const popup = L.popup();
-    marker.bindPopup(popup);
-
-    // creates field for user input
-    const input = document.createElement('input');
-    input.type = 'text';
-    input.placeholder = "Name of Toilet Location:";
-
-    // Changes popup content to be input, then opens
-    popup.setContent(input);
-    marker.openPopup();
-
-    // changes the value of popup text to what user entered
-    input.addEventListener("change", function()  {
-      const title = input.value;
-      popup.setContent(title + "<br>");
-
-      // creates remove button
-      const removeButton = document.createElement('button');
-      removeButton.textContent = "Remove";
-      removeButton.type = "button";
-      removeButton.id = "removeButtonFin" + marker._leaflet_id; //add unique ID to each removeButton
-      popup.setContent(popup.getContent() + removeButton.outerHTML);  
-     
-      let removeButtonFin = document.getElementById('removeButtonFin' + marker._leaflet_id);
-       removeButtonFin.addEventListener('click', function() {
-        marker.remove();
+    function removeMarker() {
+      const marker = this;
+      const btn = document.querySelector(".remove");
+      btn.addEventListener("click", function () {
+        mymap.removeLayer(marker);
       });
+    }
+    const buttonRemove = '<button type ="button" class="remove">Remove Toilette</button>';
+    // Create a new marker with name and coordinates
+    const marker = L.marker([lat, lng], {
+      draggable: true,
+    })
+      const popup = L.popup();
+      marker.addTo(mymap);
+      marker.bindPopup(popup);
+      let input = document.createElement('textarea');
+      // input.type = 'text';
+      input.placeholder = "Name of Toilet Location:";
+      marker.bindPopup(input);
+      marker.openPopup();
+      marker.on("popupopen", removeMarker);
+      input.addEventListener("change", function() {
+        // marker.closePopup();
+        marker.openPopup();
+        const title = input.value;
+        popup.setContent(`${title} 
+        ${buttonRemove}`);
+      })
+
+    
+
+    
+
+    // // creates popup object for marker
+    // const popup = L.popup();
+    // marker.bindPopup(popup);
+
+    // // creates field for user input
+    // const input = document.createElement('input');
+    // input.type = 'text';
+    // input.placeholder = "Name of Toilet Location:";
+
+    // // Changes popup content to be input, then opens
+    // popup.setContent(input);
+    // marker.openPopup();
+
+    // // changes the value of popup text to what user entered
+    // input.addEventListener("change", function()  {
+    //   const title = input.value;
+    //   popup.setContent(title + "<br>");
+
+    //   // creates remove button
+    //   const removeButton = document.createElement('button');
+    //   removeButton.textContent = "Remove";
+    //   removeButton.type = "button";
+    //   removeButton.id = "removeButtonFin" + marker._leaflet_id; //add unique ID to each removeButton
+    //   popup.setContent(popup.getContent() + removeButton.outerHTML);  
+     
+    //   let removeButtonFin = document.getElementById('removeButtonFin' + marker._leaflet_id);
+    //    removeButtonFin.addEventListener('click', function() {
+    //     marker.remove();
+    //   });
   
 });
 // find event listner for the marker
 //add remove function to that
-})
 }
+
 
 
 
